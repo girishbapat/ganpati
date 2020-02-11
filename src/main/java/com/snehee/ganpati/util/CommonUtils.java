274ab3fbfde9e2fc01bb99a7cb66ba0fa182acde
Girish <Girish@GIRISH>
2020-02-11 22:38:22 +0530

    Daily Booking Report WIP

--- src/main/java/com/snehee/ganpati/util/CommonUtils.java
+++ src/main/java/com/snehee/ganpati/util/CommonUtils.java
@@ -9,13 +9,14 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * Class provides common utility functions
+ * 
  * @author Bapat_G
  *
  */
 public class CommonUtils {
 
 	public static String getCurrentDateAndTime() {
-		final DateTimeFormatter timeStampPattern = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		final DateTimeFormatter timeStampPattern = DateTimeFormatter.ofPattern("ddMMMyy_HH_mm");
 		return timeStampPattern.format(LocalDateTime.now(ZoneId.of("Asia/Kolkata")));
 	}
 }
